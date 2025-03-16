40accdae960987a415ea9fd686e3327594356c7f
Randall Eike <randall@eikeconsulting.com>
2025-03-16 15:44:05 -0700

    Updated the Admin config to use the Developer Exception page when in Development mode, otherwise the somewhat more friendly (but ugly) error page.

--- src/SocketheadCleanArch.Admin/Program.cs
+++ src/SocketheadCleanArch.Admin/Program.cs
@@ -83,14 +83,15 @@ app.UseSerilogRequestLogging(options =>
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
 }
 
-app.UseExceptionHandler("/Home/Error");
 app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 app.UseHttpsRedirection();
